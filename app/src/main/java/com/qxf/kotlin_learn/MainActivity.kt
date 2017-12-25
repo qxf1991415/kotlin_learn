8312b2d93fd089a62137ca84dc90f40a97092975
qxf1991415 <quanxiaofeng.xian@haier.com>
2017-12-25 17:52:14 +0800

    第二次提交
    Change-Id: I5d6dc4141f5c67a894cc8c91fad78dd26a611b5c

--- app/src/main/java/com/qxf/kotlin_learn/MainActivity.kt
+++ app/src/main/java/com/qxf/kotlin_learn/MainActivity.kt
@@ -2,11 +2,16 @@ package com.qxf.kotlin_learn
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val a:Int = 3
+        val b = 4
+        System.out.println("$a 和 $b 的和是：${a+b}")
+        tv.text = "$a 和 $b 的和是：${a+b}"
     }
 }
